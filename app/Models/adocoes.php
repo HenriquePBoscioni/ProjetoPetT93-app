dc475b491e54eed2683ddaccc987d23dffc1f2d4
HenriquePBoscioni <henriquepra10@gmail.com>
2023-09-21 19:33:23 -0300

    .

--- app/Models/adocoes.php
+++ app/Models/adocoes.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class adocoes extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'adocoes';
+    protected $primaryKey = 'id_adocao';
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'dt_inicio',
+        'dt_inicio_pa',
+        'dt_devolucao',
+    ];
+    protected $fillabel = [
+        'id_cliente',
+        'id_pet',
+        'id_status',
+        'dt_inicio',
+        'dt_inicio_pa',
+        'dt_devolucao',
+        'descricao',
+        'observacao'
+    ];
+
 }
