f31b4c829707231f9e5e964f9d172415e7272bc7
Rafael Jesus <rafael.jesus17@hotmail.com>
2023-09-22 19:36:08 -0300

    Historico

---
+++ app/Models/HistoricoAdocao.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use App\Models\adocoes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class HistoricoAdocao extends Model
+{
+    use HasFactory , SoftDeletes;
+
+    protected $table = 'historico_adocao';
+    protected $primarykey = 'id_historico_adocao';
+    protected $dates = [
+        'created_at',
+        'update_at',
+        'deleted_at',
+
+    ];
+    protected $fillabel = [
+        'id_historico_adocacao',
+        'id_adocao',
+        'dt',
+        'historico'
+    ];
+
+
+    /**
+     * Get the user that owns the historico_adocao
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function adocao(): BelongsTo
+    {
+        return $this->belongsTo(adocoes::class, 'id_adocao', 'id_adocao');
+    }
+}
