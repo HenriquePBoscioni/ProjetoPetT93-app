4dfe0d3e108bc43d5d64ed7660f4c2fecc49a535
Lucas0604 <lucas.scavalcante1304@gmail.com>
2023-09-22 21:14:38 -0300

    teste3

--- database/migrations/2023_09_20_214130_create_adocoes_table.php
+++ database/migrations/2023_09_20_214130_create_adocoes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('adocoes', function (Blueprint $table) {
+        Schema::create('Adocoes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('adocoes');
+        Schema::dropIfExists('Adocoes');
     }
 };
